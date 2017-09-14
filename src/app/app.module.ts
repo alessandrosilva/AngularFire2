9bd6205646c340440dd98dde9d39f58bb881b3ce
alessandrosilva <alessandro.silva@gmail.com>
2017-09-14 08:58:55 -0300

    'novo'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,20 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+ 
+// AF2 Settings
+export const firebaseConfig = {
+    apiKey: "AIzaSyD6EdcPu_xLJPW97Z_p3roO0aACuJQPj3c",
+    authDomain: "kdvc-4c978.firebaseapp.com",
+    databaseURL: "https://kdvc-4c978.firebaseio.com",
+    projectId: "kdvc-4c978",
+    storageBucket: "kdvc-4c978.appspot.com",
+    messagingSenderId: "733661685846"
+};
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +28,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
